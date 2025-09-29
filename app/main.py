733ecec0e0ad1cf482c9818247c4406768f5b63a
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-29 17:20:25 +0200

    mes 2

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 class Animal:
     alive = []
+
     def __init__(self, name: str) -> None:
         self.name = name
         self.health = 100
@@ -10,7 +11,11 @@ class Animal:
         self.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
